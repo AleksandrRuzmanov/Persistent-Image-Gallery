1a1cab5efd6eee7af3b17613cd4d7eed8ddb464d
Aleksandr Ruzmanov <alexander_ruzmanov@mail.ru>
2019-03-05 15:01:37 +0300

    GalleryColletionViewCell changes saved

--- Persistent Image Gallery/GalleryCollection/GalleryCollectionViewCell.swift
+++ Persistent Image Gallery/GalleryCollection/GalleryCollectionViewCell.swift
@@ -10,82 +10,31 @@ import UIKit
 
 class GalleryCollectionViewCell: UICollectionViewCell {
     
-    
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     @IBOutlet weak var imageView: UIImageView!
-    
-    var cellWidth: CGFloat = 128.0
-    
-    var cache: URLCache?
-    
-    var imageData: Data?
-    
-    var imageURL: URL?
     
     var aspectRatio: CGFloat? {
         didSet {
             if self.aspectRatio != nil {
                 var cellFrameSize = self.frame.size
-                cellFrameSize.width = cellWidth
                 cellFrameSize.height = cellFrameSize.width * self.aspectRatio!
                 self.frame.size = cellFrameSize
-                imageView.sizeToFit()
             }
         }
     }
     
-    func setupImageViewFor(_ item: GalleryItem, width: CGFloat, using cache: URLCache) {
-        self.cache = cache
-        let session = URLSession(configuration: .default)
+    func setImageFor(_ item: GalleryItem,  using imageFetcher: ImageFetcher) {
         activityIndicator.startAnimating()
-        imageURL = nil
         aspectRatio = CGFloat(item.aspectRatio)
-        cellWidth = width
-        if let url = item.url?.imageURL {
-            imageURL = url
-        } else if let data = item.imageData {
-            imageData = data
-        }
-        
-        aspectRatio = CGFloat(item.aspectRatio)
-        if imageURL != nil {
-            let request = URLRequest(url: imageURL!)
-            if let cashedResponse = cache.cachedResponse(for: request), let image = UIImage(data: cashedResponse.data) {
-                // if image is in cache
-                imageView.image = image
-                let aspectRatio = image.size.height / image.size.width
-                self.aspectRatio = aspectRatio
-                activityIndicator.stopAnimating()
+        imageFetcher.fetchImageFor(item) { [weak self] (image) in
+            if image != nil {
+                self?.imageView.image = image
+                self?.aspectRatio = image!.size.height / image!.size.width
             } else {
-                // if image isn't in cache
-                DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-                    let task = session.dataTask(with: request, completionHandler: { (urlData, urlResponse, urlError) in
-                        DispatchQueue.main.async {
-                            if urlError != nil {
-                                print("Data request failed with error \(urlError!)")
-                            }
-                            if let data = urlData, let image = UIImage(data: data) {
-                                if let response = urlResponse {
-                                    self?.cache?.storeCachedResponse(CachedURLResponse(response: response, data: data), for: request)
-                                }
-                                self?.imageView.image = image
-                                let aspectRatio = image.size.height / image.size.width
-                                self?.aspectRatio = aspectRatio
-                            } else {
-                                self?.backgroundColor = UIColor.red
-                            }
-                            self?.activityIndicator.stopAnimating()
-                        }
-                    })
-                    task.resume()
-                }
+                self?.imageView.backgroundColor = UIColor.red
             }
-        } else if imageData != nil, let image = UIImage(data: imageData!) {
-            imageView.image = image
-            let aspectRatio = image.size.height / image.size.width
-            self.aspectRatio = aspectRatio
-            self.activityIndicator.stopAnimating()
+            self?.activityIndicator.stopAnimating()
         }
     }
 }
